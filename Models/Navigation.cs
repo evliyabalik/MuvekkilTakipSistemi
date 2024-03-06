cd1f5935bd87f9e04a3e1d6f24324c3a20a592b2
evliyabalik <evliyabalik@gmail.com>
2024-03-07 02:48:22 +0300

    -Kullanıcılar ne diyor kısmına kadar düzenlemeler yapıldı. -Banner değiştirildi -Hoş geldiniz slider kaldırıldı, yazı sisteme uyduruldu -Ne yapıyoruz, png resimleri değiştirildi -Css üzerinden ufak değişiklikler yapıldı. -Hakkımızda menüsü ve sayfası eklendi -Menüler için model oluşturuldu ve menü ihtiyaçlarına göre propertyler eklendi.

---
+++ Models/Navigation.cs
@@ -0,0 +1,10 @@
+﻿namespace MuvekkilTakipSistemi.Models
+{
+	public class Navigation
+	{
+		public string MenuText { get; set; }
+		public string ActionName { get; set; }
+		public string ControlName { get; set; }
+		
+	}
+}
