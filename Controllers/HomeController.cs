77d469af911309b07dc71284fa3cc27ca4068307
evliyabalik <evliyabalik@gmail.com>
2024-03-10 03:23:46 +0300

    -Tasarımsal olarak sayfa tamamlandı. -Şifremi unuttum sayfası eklendi -Basit bazı düzenlemeler.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,7 +37,12 @@ namespace MuvekkilTakipSistemi.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult ForgotPass()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
