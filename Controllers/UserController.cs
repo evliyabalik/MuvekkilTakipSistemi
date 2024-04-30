c467befabbe84da4611d42aa7082285cf5cc90d4
evliyabalik <evliyabalik@gmail.com>
2024-04-30 04:35:03 +0300

    -Session Clear eklendi (Çıkış yap) -User Panel Geliştirildi -Müvekkiller, Dosyalar ve İşlemler eklendi. -User butonuna Ayarlar ve Çıkış yap eklendi. -Menüler Düzenlendi -Sağ menü eklendi -Footer kısmı içerikleri komple kaldırıldı. -İletişim maili değiştirildi.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,7 +11,7 @@ namespace MuvekkilTakipSistemi.Controllers
 			var bSicilNo = HttpContext.Session.GetString("BSicilNo");
 			var userId = HttpContext.Session.GetString("UserId");
 
-			ViewData["isim"] = adsoyad;
+			TempData["isim"] = adsoyad;
 
 			if (adsoyad != null && bSicilNo != null && userId != null)
 				return View();
@@ -21,5 +21,68 @@ namespace MuvekkilTakipSistemi.Controllers
 					"Lütfen bilgilerinizi kontrol edip tekrar deneyiniz.");
 			}
 		}
+
+        [HttpGet]
+        public IActionResult Files()
+        {
+            var adsoyad = HttpContext.Session.GetString("Adsoyad");
+            var bSicilNo = HttpContext.Session.GetString("BSicilNo");
+            var userId = HttpContext.Session.GetString("UserId");
+
+            TempData["isim"] = adsoyad;
+
+            if (adsoyad != null && bSicilNo != null && userId != null)
+                return View();
+            else
+            {
+                return Content("Bu sayfaya erişim izniniz bulunmamaktadır. " +
+                    "Lütfen bilgilerinizi kontrol edip tekrar deneyiniz.");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Activities()
+        {
+            var adsoyad = HttpContext.Session.GetString("Adsoyad");
+            var bSicilNo = HttpContext.Session.GetString("BSicilNo");
+            var userId = HttpContext.Session.GetString("UserId");
+
+            TempData["isim"] = adsoyad;
+
+            if (adsoyad != null && bSicilNo != null && userId != null)
+                return View();
+            else
+            {
+                return Content("Bu sayfaya erişim izniniz bulunmamaktadır. " +
+                    "Lütfen bilgilerinizi kontrol edip tekrar deneyiniz.");
+            }
+        }
+
+		[HttpGet]
+		public IActionResult Settings()
+		{
+			var adsoyad = HttpContext.Session.GetString("Adsoyad");
+			var bSicilNo = HttpContext.Session.GetString("BSicilNo");
+			var userId = HttpContext.Session.GetString("UserId");
+
+			TempData["isim"] = adsoyad;
+
+			if (adsoyad != null && bSicilNo != null && userId != null)
+				return View();
+			else
+			{
+				return Content("Bu sayfaya erişim izniniz bulunmamaktadır. " +
+					"Lütfen bilgilerinizi kontrol edip tekrar deneyiniz.");
+			}
+		}
+
+		public IActionResult Exit()
+		{
+			HttpContext.Session.Clear();
+			return RedirectToAction("Login", "Home");
+		}
+
+
+
 	}
 }
